acc570607c5e3e999eb319f4e0174533dda9d2e4
SyuyaNakayama <k021g1352@g.neec.ac.jp>
2023-09-07 12:33:00 +0900

    GameSceneの処理をStageに移動

--- SourceFiles/Application/Stage/Stage.cpp
+++ SourceFiles/Application/Stage/Stage.cpp
@@ -1,11 +1,215 @@
 #include "Stage.h"
+#include <imgui.h>
+#include "Timer.h"
 
 void Stage::Initialize()
 {
-	block.Initialize();
+	//block.Initialize();
+
+	for (int y = 0; y < 22; y++) {
+		for (int x = 0; x < 12; x++) {
+			blocks[y][x] = ModelManager::Create("cube");
+			blocks[y][x]->worldTransform->scale *= 4.8f;
+			blocks[y][x]->worldTransform->translation = { -((float)x - 12 / 2) * 5 * 2, -((float)y - 22 / 2) * 5 * 2, 0.0f };
+		}
+	}
+	Mtimer = 1;
+	Mtimer.Start();
 }
 
 void Stage::Update()
 {
-	block.Update();
+	//block.Update();
+
+	ImGui::Text("IsHit:%d", IsHit(minoX, minoY + 1, minoType, minoAngle));
+	ImGui::Text("deleteNum:%d", deleteNum);
+	ImGui::Text("sam:%d", sam);
+	/*for (int i = 0; i < 22; i++) {
+		ImGui::Text("%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d",
+			field[i][0], field[i][1], field[i][2], field[i][3], field[i][4], field[i][5],
+			field[i][6], field[i][7], field[i][8], field[i][9], field[i][10], field[i][11]);
+	}
+
+	ImGui::Text("----------------------------------");
+
+	for (int i = 0; i < 22; i++) {
+		ImGui::Text("%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d",
+			displayBuffer[i][0], displayBuffer[i][1], displayBuffer[i][2], displayBuffer[i][3], displayBuffer[i][4],  displayBuffer[i][5],
+			displayBuffer[i][6], displayBuffer[i][7], displayBuffer[i][8], displayBuffer[i][9], displayBuffer[i][10], displayBuffer[i][11]);
+	}*/
+
+
+
+	//òg
+	/*for (int i = 0; i < FIELD_HEIGHT; ++i)
+	{
+		field[i][0] = 1;
+		field[i][FIELD_WIDTH - 1] = 1;
+	}*/
+
+	for (int i = 0; i < FIELD_WIDTH; ++i)
+	{
+		field[FIELD_HEIGHT - 1][i] = 1;
+	}
+
+	if (isflag == true)
+	{
+		if (input->IsInput(Key::S)) {
+			if (input->IsTrigger(Key::S)) { fallTimer.Start(); }
+			if (fallTimer.Update() && !IsHit(minoX, minoY + 1, minoType, minoAngle))
+			{
+				++minoY;
+			}
+		}
+
+		if (input->IsInput(Key::A)) {
+			holdTimeA++;
+
+			if (holdTimeA <= 10)
+			{
+				if (input->IsTrigger(Key::A) && !IsHit(minoX + 1, minoY, minoType, minoAngle))
+				{
+					++minoX;
+				}
+			}
+			else
+			{
+				if (!IsHit(minoX + 1, minoY, minoType, minoAngle))
+				{
+					++minoX;
+				}
+			}
+		}
+		else
+		{
+			holdTimeA = 0;
+		}
+
+		if (input->IsInput(Key::D)) {
+			holdTimeD++;
+
+			if (holdTimeD <= 10)
+			{
+				if (input->IsTrigger(Key::D) && !IsHit(minoX - 1, minoY, minoType, minoAngle))
+				{
+					--minoX;
+				}
+			}
+			else
+			{
+				if (!IsHit(minoX - 1, minoY, minoType, minoAngle))
+				{
+					--minoX;
+				}
+			}
+		}
+		else
+		{
+			holdTimeD = 0;
+		}
+
+		if (input->IsTrigger(Key::W))
+		{
+			while (!IsHit(minoX, minoY + 1, minoType, minoAngle))
+			{
+				++minoY;
+			}
+		}
+
+		if (input->IsTrigger(Key::Space)) {
+			if (!IsHit(minoX, minoY, minoType, (minoAngle + 1) % (int)MinoAngle::Max))
+			{
+				minoAngle = (minoAngle + 1) % (int)MinoAngle::Max;
+			}
+		}
+		Display();
+
+		if (Mtimer.Update()) {
+			Mtimer.Start();
+
+			//ìÆÇ©ÇµÇƒÇÈmino
+			if (IsHit(minoX, minoY + 1, minoType, minoAngle)) {
+				for (int i = 0; i < MINO_HEIGHT; ++i) {
+					for (int j = 0; j < MINO_WIDTH; ++j) {
+						field[minoY + i][minoX + j] |= minoShapes[minoType][minoAngle][i][j];
+					}
+				}
+				ResetMino();
+			}
+			else {
+				++minoY;
+			}
+
+			Display();
+		}
+	}
+
+	//ëµÇ¡ÇƒÇ¢ÇÈçsÇíTÇ∑
+	for (int i = FIELD_HEIGHT - 2; i > 0; i--) {//àÍî‘â∫ÇÕògÇÃÇΩÇﬂ
+		//â°àÍóÒÇÃçáåvÇãÅÇﬂÇÈ
+		sam[i] = field[i][0] + field[i][1] + field[i][2] + field[i][3] + field[i][4] + field[i][5] +
+			field[i][6] + field[i][7] + field[i][8] + field[i][9] + field[i][10] + field[i][11];
+
+		for (int j = 0; j < FIELD_WIDTH; ++j) {
+			if (sam[i] == 12) {//ëµÇ¡ÇΩÇÁè¡Ç∑
+				field[i][j] = 0;
+
+				for (int x = i; x > 0; x--) {
+					field[x + 1][j] = field[x][j];
+					field[x][j] = 0;
+				}
+			}
+		}
+	}
+}
+
+void Stage::Display()
+{
+	memcpy(displayBuffer, field, sizeof(field));
+
+	for (int i = 0; i < MINO_HEIGHT; ++i)
+	{
+		for (int j = 0; j < MINO_WIDTH; ++j)
+		{
+			displayBuffer[minoY + i][minoX + j] |= minoShapes[minoType][minoAngle][i][j];
+		}
+	}
+
+	for (int i = 0; i < FIELD_HEIGHT; ++i)
+	{
+		for (int j = 0; j < FIELD_WIDTH; ++j)
+		{
+			if (1 == displayBuffer[i][j])
+			{
+				blocks[i][j]->material.GetSprite(TexType::Main)->color = { 0.3f, 0.0f, 0.0f, 1.0f };
+			}
+			else
+			{
+				blocks[i][j]->material.GetSprite(TexType::Main)->color = { 0.3f, 0.3f, 0.3f, 1.0f };
+			}
+		}
+	}
+}
+
+bool Stage::IsHit(int argMinoX, int argMinoY, int argMinoType, int argMinoAngle)
+{
+	for (int i = 0; i < MINO_HEIGHT; ++i)
+	{
+		for (int j = 0; j < MINO_WIDTH; ++j)
+		{
+			if (minoShapes[argMinoType][argMinoAngle][i][j] && field[argMinoY + i][argMinoX + j])
+			{
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+void Stage::ResetMino()
+{
+	minoX = 5;
+	minoY = 0;
+	minoType = rand() % (int)MinoType::Max;
+	minoAngle = rand() % (int)MinoAngle::Max;
 }
--- SourceFiles/Application/Stage/Stage.h
+++ SourceFiles/Application/Stage/Stage.h
@@ -7,6 +7,271 @@ class Stage
 private:
 	std::array<std::array<bool, 12>, 15> stage; // 縦15 * 横12マス
 	Block block; // 現在落ちているブロック
+	static const int FIELD_WIDTH = 12;
+	static const int FIELD_HEIGHT = 22;
+
+	static const int MINO_WIDTH = 4;
+	static const int MINO_HEIGHT = 4;
+
+	Input* input = Input::GetInstance();
+
+	enum class MinoType { I, O, S, Z, J, L, T, Max };
+
+	enum class MinoAngle { _0, _90, _180, _270, Max };
+
+	int minoShapes[(int)MinoType::Max][(int)MinoAngle::Max][MINO_WIDTH][MINO_HEIGHT] =
+	{
+		// MINO_TYPE_I
+		{
+			// MINO_ANGLE_0
+			{
+				0, 1, 0, 0,
+				0, 1, 0, 0,
+				0, 1, 0, 0,
+				0, 1, 0, 0
+			},
+		// MINO_ANGLE_90
+		{
+			0, 0, 0, 0,
+			0, 0, 0, 0,
+			1, 1, 1, 1,
+			0, 0, 0, 0
+		},
+		// MINO_ANGLE_180
+		{
+			0, 0, 1, 0,
+			0, 0, 1, 0,
+			0, 0, 1, 0,
+			0, 0, 1, 0
+		},
+		// MINO_ANGLE_270
+		{
+			0, 0, 0, 0,
+			1, 1, 1, 1,
+			0, 0, 0, 0,
+			0, 0, 0, 0
+		}
+	},
+
+		// MINO_TYPE_O
+		{
+			// MINO_ANGLE_0
+			{
+				0, 0, 0, 0,
+				0, 1, 1, 0,
+				0, 1, 1, 0,
+				0, 0, 0, 0
+			},
+		// MINO_ANGLE_90
+		{
+			0, 0, 0, 0,
+			0, 1, 1, 0,
+			0, 1, 1, 0,
+			0, 0, 0, 0
+		},
+		// MINO_ANGLE_180
+		{
+			0, 0, 0, 0,
+			0, 1, 1, 0,
+			0, 1, 1, 0,
+			0, 0, 0, 0
+		},
+		// MINO_ANGLE_270
+		{
+			0, 0, 0, 0,
+			0, 1, 1, 0,
+			0, 1, 1, 0,
+			0, 0, 0, 0
+		}
+	},
+
+		// MINO_TYPE_S
+		{
+			// MINO_ANGLE_0
+			{
+				0, 0, 0, 0,
+				0, 1, 1, 0,
+				1, 1, 0, 0,
+				0, 0, 0, 0
+			},
+		// MINO_ANGLE_90
+		{
+			0, 1, 0, 0,
+			0, 1, 1, 0,
+			0, 0, 1, 0,
+			0, 0, 0, 0
+		},
+		// MINO_ANGLE_180
+		{
+			0, 0, 0, 0,
+			0, 1, 1, 0,
+			1, 1, 0, 0,
+			0, 0, 0, 0
+		},
+		// MINO_ANGLE_270
+		{
+			0, 0, 0, 0,
+			0, 1, 0, 0,
+			0, 1, 1, 0,
+			0, 0, 1, 0
+		}
+	},
+
+		// MINO_TYPE_Z
+		{
+			// MINO_ANGLE_0
+			{
+				0, 0, 0, 0,
+				1, 1, 0, 0,
+				0, 1, 1, 0,
+				0, 0, 0, 0
+			},
+		// MINO_ANGLE_90
+		{
+			0, 0, 0, 0,
+			0, 0, 1, 0,
+			0, 1, 1, 0,
+			0, 1, 0, 0
+		},
+		// MINO_ANGLE_180
+		{
+			0, 0, 0, 0,
+			0, 1, 1, 0,
+			0, 0, 1, 1,
+			0, 0, 0, 0
+		},
+		// MINO_ANGLE_270
+		{
+			0, 0, 1, 0,
+			0, 1, 1, 0,
+			0, 1, 0, 0,
+			0, 0, 0, 0
+		}
+	},
+
+		// MINO_TYPE_J
+		{
+			// MINO_ANGLE_0
+			{
+				0, 0, 1, 0,
+				0, 0, 1, 0,
+				0, 1, 1, 0,
+				0, 0, 0, 0
+			},
+		// MINO_ANGLE_90
+		{
+			0, 0, 0, 0,
+			1, 1, 1, 0,
+			0, 0, 1, 0,
+			0, 0, 0, 0
+		},
+		// MINO_ANGLE_180
+		{
+			0, 0, 0, 0,
+			0, 1, 1, 0,
+			0, 1, 0, 0,
+			0, 1, 0, 0
+		},
+		// MINO_ANGLE_270
+		{
+			0, 0, 0, 0,
+			0, 1, 0, 0,
+			0, 1, 1, 1,
+			0, 0, 0, 0
+		}
+	},
+
+		// MINO_TYPE_L
+		{
+			// MINO_ANGLE_0
+			{
+				0, 1, 0, 0,
+				0, 1, 0, 0,
+				0, 1, 1, 0,
+				0, 0, 0, 0
+			},
+		// MINO_ANGLE_90
+		{
+			0, 0, 0, 0,
+			0, 0, 1, 0,
+			1, 1, 1, 0,
+			0, 0, 0, 0
+		},
+		// MINO_ANGLE_180
+		{
+			0, 0, 0, 0,
+			0, 1, 1, 0,
+			0, 0, 1, 0,
+			0, 0, 1, 0
+		},
+		// MINO_ANGLE_270
+		{
+			0, 0, 0, 0,
+			0, 1, 1, 1,
+			0, 1, 0, 0,
+			0, 0, 0, 0
+		}
+	},
+
+		// MINO_TYPE_T
+		{
+			// MINO_ANGLE_0
+			{
+				0, 0, 0, 0,
+				1, 1, 1, 0,
+				0, 1, 0, 0,
+				0, 0, 0, 0
+			},
+		// MINO_ANGLE_90
+		{
+			0, 0, 0, 0,
+			0, 1, 0, 0,
+			0, 1, 1, 0,
+			0, 1, 0, 0
+		},
+		// MINO_ANGLE_180
+		{
+			0, 0, 0, 0,
+			0, 0, 1, 0,
+			0, 1, 1, 1,
+			0, 0, 0, 0
+		},
+		// MINO_ANGLE_270
+		{
+			0, 0, 1, 0,
+			0, 1, 1, 0,
+			0, 0, 1, 0,
+			0, 0, 0, 0
+		}
+	}
+	};
+
+	int minoType = 0;
+	int minoAngle = 0;
+	int minoX = 0;
+	int minoY = 0;
+
+	bool isflag = true;
+	//bool isLineFilled = true;
+	RealTimer Mtimer;
+	RealTimer fallTimer = 0.03f;
+	int holdTimeA = 0;
+	int holdTimeD = 0;
+	//　　　　　　　　y   x
+	Object3d* blocks[22][12];
+
+	int field[FIELD_HEIGHT][FIELD_WIDTH] = { 0 };
+
+	int displayBuffer[FIELD_HEIGHT][FIELD_WIDTH] = { 0 };
+
+	int fall[FIELD_HEIGHT][FIELD_WIDTH] = { 0 };
+
+	int sam[21];
+	int deleteNum;
+
+	void Display();
+	bool IsHit(int argMinoX, int argMinoY, int argMinoType, int argMinoAngle);
+	void ResetMino();
 
 public:
 	void Initialize();
