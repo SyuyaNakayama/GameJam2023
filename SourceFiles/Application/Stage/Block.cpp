b1640ccb93ce69a2111e770b5275e1e78dfd345e
KoRoTa-school@outlook.jp <KoRoTa-school@outlook.jp>
2023-09-10 01:14:51 +0900

    モデルの変更と位置微調整

--- SourceFiles/Application/Stage/Block.cpp
+++ SourceFiles/Application/Stage/Block.cpp
@@ -5,6 +5,6 @@ void Block::Initialize(float x, float y)
 	//‰~’Œ‚É‚·‚é
 	object = ModelManager::Create("block");
 	object->worldTransform->scale *= 10.0f;
-	object->worldTransform->translation = { 0.0f, -(float)y * 5.0f , 0.0f };
+	object->worldTransform->translation = { 0.0f, -(float)y * 4.8f+55 , 0.0f };
 	object->worldTransform->rotation.y = x * Angle(30);
 }
