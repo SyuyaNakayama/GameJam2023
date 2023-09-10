d632d726c77bcc7e39098f3224c8a7ae119e60f7
KoRoTa-school@outlook.jp <KoRoTa-school@outlook.jp>
2023-09-10 10:18:12 +0900

    とりあえずのスコアと終了フラグ追加

--- SourceFiles/Application/Stage/Block.cpp
+++ SourceFiles/Application/Stage/Block.cpp
@@ -2,9 +2,16 @@
 
 void Block::Initialize(float x, float y)
 {
+	//平面にする(デバック用...残せ)
+	object = ModelManager::Create("cube");
+	object->worldTransform->scale *= 5.0f;
+	object->worldTransform->translation =
+	{ -((float)x - 22 / 2) * 10, -((float)y - 12 / 2) * 10, 0.0f };
+	
+
 	//円柱にする
-	object = ModelManager::Create("block");
+	/*object = ModelManager::Create("block");
 	object->worldTransform->scale *= 10.0f;
 	object->worldTransform->translation = { 0.0f, -(float)y * 4.8f+55 , 0.0f };
-	object->worldTransform->rotation.y = x * Angle(30);
+	object->worldTransform->rotation.y = x * Angle(30);*/
 }
